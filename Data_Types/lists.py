8564b6dbf488a38ab70c5989465878cbc7431498
cjwoodruff <30675608+cjwoodruff@users.noreply.github.com>
2017-11-08 01:51:05 -0600

    Create lists.py

---
+++ Data_Types/lists.py
@@ -0,0 +1,22 @@
+x = [1, 2, 3, 'Jon', 'Snow', True]
+print(x)
+
+# Displaying the indexing property of lists
+print(x[0])
+print(x[4])
+
+# Displaying the append() method
+x.append(4)
+x.append(5)
+x.append(6)
+print(x)
+
+# Displaying the pop() method
+x.pop()
+print(x)
+
+# Display the del method
+del x[3:5]
+print(x)
+del x[3]
+print(x)
