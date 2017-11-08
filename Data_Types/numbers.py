1ed47513bb11e3840c3188021578e3ed34d6ac07
cjwoodruff <30675608+cjwoodruff@users.noreply.github.com>
2017-11-08 01:44:24 -0600

    Update numbers.py

--- Data_Types/numbers.py
+++ Data_Types/numbers.py
@@ -1,1 +1,23 @@
+# This is for Python Introduction Part 2 - Data Types tutorial series
+# This is a copy of the code found within the tutorial. If it is run as-is,
+# it may come with some strange results.
 
+x = 'Hello Python!'
+print(x)
+
+# or
+
+x = "Hello Python!"
+print(x)
+
+# This is the model for the single vs double quote.
+x = “This is a 'quote' using single quotes inside.”
+y = 'This is a “quote” using double quotes inside.'
+print(x)
+print(y)
+
+# Triple quote model
+x = '''This is a triple quote that I
+can write whatever I want
+and use as many lines as I want.'''
+print(x)
