720a6ccf3bfb2c6bf44918dc334daeee57ec2888
cjwoodruff <30675608+cjwoodruff@users.noreply.github.com>
2017-11-08 01:33:05 -0600

    Create booleans.py
    Source code for Data-Types -Booleans section of tutorials

---
+++ Data_Types/booleans.py
@@ -0,0 +1,24 @@
+# This is for Python Introduction Part 2 - Data Types tutorial series
+# This is a copy of the code found within the tutorial. If it is run as-is,
+# it may come with some strange results.
+
+# Set up the initial variables for comparison
+x = 5 
+y = 6
+
+# Initial boolean comparisons
+print(y > x) # Output: True
+print(y < x) # Output: False
+
+# if statement
+if y > x:
+    print(True)
+else:
+    print(False)
+    
+# THe following code is modified to be placed into an IDE and not the console
+# by using the print() method.
+print(True + True) # Output: 2
+print(True * True) # Output: 1
+print(True - False) # Output: 1
+pritn(False/True) # Output: 0
