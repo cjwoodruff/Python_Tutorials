706d40728a6e21a16d28da663f5f91335078f013
cjwoodruff <30675608+cjwoodruff@users.noreply.github.com>
2017-11-08 03:56:51 -0600

    Create while_loop.py

---
+++ Loops/while_loop.py
@@ -0,0 +1,21 @@
+# Using a while loop, iterate x down from 10 to 0
+x = 10
+
+while x > 0:
+    print(x + ' is greater than zero')
+    x--
+
+print('x is now equal to 0')
+
+# Ask user for input and print the input if is isn't # which continues
+# or 'done' which ends the loop
+print("Type 'done' when you want to leave the loop.")
+while True:
+    name = input('Please enter your name: ')
+    if name == '#'
+        continue
+    if name == 'done'
+        break
+    print(name)
+
+print('Done!')
